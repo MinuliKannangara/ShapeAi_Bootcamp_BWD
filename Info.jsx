2121e904f5940439878183f7f368a8267a81c09a
MinuliKannangara <73284411+MinuliKannangara@users.noreply.github.com>
2021-06-27 03:49:56 -0700

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This ws an amazing Bootcmp taken by Saurya Sinha. We coverd everything from
+        scratch including Javascript React.js,HTML
+      </p>
+    </div>
+   
+  );
+}
+
+export default Info;
